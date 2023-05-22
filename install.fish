28719cb979f62afe676a1020c1cb98cb080c60e6
D. Bohdan <dbohdan@dbohdan.com>
2023-05-22 16:07:39 +0000

    feat(install): add `install.fish`

---
+++ install.fish
@@ -0,0 +1,12 @@
+#! /usr/bin/env fish
+
+cd "$(path dirname "$(status filename)")"
+
+set --local src fenvdir.fish
+set --local dst $__fish_config_dir/functions/fenvdir.fish
+
+if not cp $src $dst
+    return 1
+end
+
+printf 'copied "%s" to "%s"\n' $src $dst
